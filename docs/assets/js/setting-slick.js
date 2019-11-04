ffbb44f2e5428f5d58b082760239472165c6fbb9
Gladibeth <gladibethdelos@gmail>
2019-11-04 10:05:29 -0400

    Add views index, company, software, hardware, pricing and contact, and resposive all views

--- docs/assets/js/setting-slick.js
+++ docs/assets/js/setting-slick.js
@@ -4,7 +4,7 @@ $('.main-banner__content').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
   dots: false,
-  arrows: false,
+  arrows: true,
   responsive: [{
       breakpoint: 1200,
       settings: {
@@ -27,6 +27,45 @@ $('.main-banner__content').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
         dots: false,
+        autoplay: false,
+        arrows: false,
+        autoplaySpeed: 1000
+      }
+    }
+  ]
+});
+
+
+
+$('.main-testimonials__content').slick({
+  infinite: true,
+  autoplay: false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: true,
+  arrows: false,
+  responsive: [{
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
         autoplay: false,
         arrows: false,
         autoplaySpeed: 1000
