8369f8e180760f50fc5020216e457fb2963b8294
Gladibeth <gladibethdelos@gmail>
2019-11-08 10:16:53 -0400

    Add Change wp_config

--- wp-config.php
+++ wp-config.php
@@ -26,16 +26,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'Q8zhVVYWU7' );
+define( 'DB_NAME', 'alocity' );
 
 /** MySQL database username */
-define( 'DB_USER', 'Q8zhVVYWU7' );
+define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'ivlfEV6Z3A' );
+define( 'DB_PASSWORD', 'root' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'remotemysql.com' );
+define( 'DB_HOST', 'localhost' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
