15bdda247da389766c16236e60e0f6dd345f6b7f
Misledy <misledy1@hotmail.com>
2019-11-11 10:19:11 -0400

    Add manageable main-banner

--- wp-content/themes/Alocity/sections/main-banner.php
+++ wp-content/themes/Alocity/sections/main-banner.php
@@ -1,32 +1,23 @@
   <div class="main-banner">
     <div class="main-banner__content">
+    <?php for ($i=1; $i < 4; $i++) { ?>
+      <?php if (get_theme_mod('banner'.$i.'_title')!= NULL ) { ?>
+
       <div class="main-banner__item">
         <div class="mask mask--home">
           <div class="main-banner__text main-banner__text--home">
             <div class="main-banner__title">
-              <h1>Simply Smart Segurity</h1>
-              <span>Cloud Access Control Software</span>
+              <h1><?php echo get_theme_mod('banner'.$i.'_title'); ?></h1>
+              <span><?php echo get_theme_mod('banner'.$i.'_subtitle'); ?></span>
             
             </div>
           </div>
         </div>
         <div class="main-banner__img">
-          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/index/banner.jpg">
+          <img src="<?php echo get_theme_mod('banner'.$i.'_image'); ?>">
         </div>
       </div>
-      <div class="main-banner__item">
-        <div class="mask mask--home">
-          <div class="main-banner__text main-banner__text--home">
-            <div class="main-banner__title">
-              <h1>Simply Smart Segurity</h1>
-              <span>Cloud Access Control Software</span>
-              
-            </div>
-          </div>
-        </div>
-        <div class="main-banner__img">
-          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/index/banner.jpg">
-        </div>
-      </div>
+    <?php }} ?>   
     </div>
   </div>
+
