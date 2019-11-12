09f70c175adb20e6d28c5a6a95facfaa7218eb25
Misledy <misledy1@hotmail.com>
2019-11-12 15:09:51 -0400

    Add manageable section main-readers

--- wp-content/themes/Alocity/sections/main-readers.php
+++ wp-content/themes/Alocity/sections/main-readers.php
@@ -1,111 +1,60 @@
-  <div class="main-readers">
+  <div class="main-readers" style="background:<?php echo get_theme_mod('background_readers'); ?>;">
     <div class="container">
       <div class="main-title__general main-title__general--flex">
-        <h2>All-in-one Smart Door Readers</h2>
-        <p>Our smart readers are truly smart, connecting directly to the cloud and door locks, and do not require the need for costly controller panels.</p>
+        <h2><?php echo get_theme_mod('readers_title'); ?></h2>
+        <p><?php echo get_theme_mod('readers_content'); ?></p>
       </div>
       <div class="main-readers__content">
         <div class="main-readers__item">
+        <?php for ($i=1; $i <=3 ; $i++) { 
+          if(get_theme_mod('readers'.$i.'_title')!=NULL){?>
           <div class="main-readers__box">
             <div class="main-users__boxtext main-users__boxtext--right">
               <div class="main-users__title">
-                <p>#1 Fingerprint Technology</p>
+                <p><?php echo get_theme_mod('readers'.$i.'_title'); ?></p>
               </div>
               <div class="main-general__description main-general__description--readers">
-                <p>Reads fingerprint from anyone, and in any environmental condition</p>
+                <p><?php echo get_theme_mod('readers'.$i.'_content'); ?></p>
               </div>
             </div>
             <div class="main-users__boximg active">
               <div class="main-users__img">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/index/fingerprint.png">
+                <img src="<?php echo get_theme_mod('users'.$i.'_icon'); ?>">
               </div>
             </div>
           </div>
-          <div class="main-readers__box">
-            <div class="main-users__boxtext main-users__boxtext--right">
-              <div class="main-users__title">
-                <p>Multi Smart-Card Reader</p>
-              </div>
-              <div class="main-general__description main-general__description--readers">
-                <p>Readers can be configured to read Mifare, Desfire, NFC, & HID</p>
-              </div>
-            </div>
-            <div class="main-users__boximg active">
-              <div class="main-users__img">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/index/name.png">
-              </div>
-            </div>
-          </div>
-          <div class="main-readers__box">
-            <div class="main-users__boxtext main-users__boxtext--right">
-              <div class="main-users__title">
-                <p>Alocity® Mobile Credentials</p>
-              </div>
-              <div class="main-general__description main-general__description--readers">
-                <p>Give users the flexibility to open doors using their smartphone</p>
-              </div>
-            </div>
-            <div class="main-users__boximg active">
-              <div class="main-users__img">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/index/smartphone-2.png">
-              </div>
-            </div>
-          </div>
+        <?php }} ?> 
         </div>
+
         <div class="main-readers__item">
           <div class="main-readers__img">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/index/image_3.png">
+            <img src="<?php echo get_theme_mod('readers_image'); ?>">
           </div>
           <div class="main-check__btn">
-            <a class="btn_custom btn--small btn--filled btn--filled--upper" href="">Check Out Our Door Readers</a>
+            <a class="btn_custom btn--small btn--filled btn--filled--upper" href="<?php echo get_theme_mod('readers_urlbutton'); ?>"><?php echo get_theme_mod('readers_button'); ?></a>
           </div>
         </div>
+
+
         <div class="main-readers__item">
+        <?php for ($i=4; $i <=6 ; $i++) { 
+          if(get_theme_mod('readers'.$i.'_title')!=NULL){?>        
           <div class="main-readers__box main-readers__box--segund">
             <div class="main-users__boximg active">
               <div class="main-users__img">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/index/fast.png">
+                <img src="<?php echo get_theme_mod('readers'.$i.'_icon'); ?>">
               </div>
             </div>
             <div class="main-users__boxtext">
               <div class="main-users__title">
-                <p>Installs in minutes</p>
+                <p><?php echo get_theme_mod('readers'.$i.'_title'); ?></p>
               </div>
               <div class="main-general__description main-general__description--readers">
-                <p>No configuration or cumbersome wiring required. Syncs with cloud in seconds</p>
+                <p><?php echo get_theme_mod('readers'.$i.'_content'); ?></p>
               </div>
             </div>
           </div>
-          <div class="main-readers__box main-readers__box--segund">
-            <div class="main-users__boximg active">
-              <div class="main-users__img">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/index/hydro-power.png">
-              </div>
-            </div>
-            <div class="main-users__boxtext">
-              <div class="main-users__title">
-                <p>Weatherproof</p>
-              </div>
-              <div class="main-general__description main-general__description--readers">
-                <p>Designed with an IP-67 rated housing ideal for extreme outdoor environments, working even under water, snow, dust, and humid conditions. (AC500/AC500 Models)</p>
-              </div>
-            </div>
-          </div>
-          <div class="main-readers__box main-readers__box--segund">
-            <div class="main-users__boximg active">
-              <div class="main-users__img">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/index/shield.png">
-              </div>
-            </div>
-            <div class="main-users__boxtext">
-              <div class="main-users__title">
-                <p>Vandal Resistant</p>
-              </div>
-              <div class="main-general__description main-general__description--readers">
-                <p>Engineered to withstand a high level of impact</p>
-              </div>
-            </div>
-          </div>
+        <?php }} ?> 
         </div>
       </div>
     </div>
